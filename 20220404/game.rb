dcfe50b7d6cb335839ce79a67acb5023a13fe1ca
Masahito Osako <velonica.gale2227@gmail.com>
2022-04-05 09:00:50 +0900

    update

---
+++ 20220404/game.rb
@@ -0,0 +1,33 @@
+class Game
+  def initialize
+    @score = 0
+    @rolls = []
+    @current_roll = 0
+  end
+
+  def roll(pins)
+    @rolls[@current_roll] = pins
+    @current_roll += 1
+  end
+
+  def score
+    score = 0
+    roll_index = 0
+    10.times do
+      frame_score = @rolls[roll_index] + @rolls[roll_index + 1]
+      if spare? roll_index
+        score += frame_score + @rolls[roll_index + 2]
+      else
+        score += frame_score
+      end
+      roll_index += 2
+    end
+    score
+  end
+
+  private
+
+  def spare?(roll_index)
+    (@rolls[roll_index] + @rolls[roll_index + 1]) == 10
+  end
+end
