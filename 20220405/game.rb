33e4e5a8ff54b3b6fd594ae66a6a98714d6410e6
Masahito Osako <velonica.gale2227@gmail.com>
2022-04-06 22:55:18 +0900

    update

--- 20220405/game.rb
+++ 20220405/game.rb
@@ -4,21 +4,10 @@ module D20220405
 
     def initialize
       @score = 0
-      @rolls = []
-      @current_roll = 0
     end
 
     def roll(pins)
-      @rolls[@current_roll] = pins
-      @current_roll += 1
-    end
-
-    def score
-      roll_index = 0
-      score = 0
-      10.times do
-
-      end
+      @score += pins
     end
   end
 end
--- 20220405/game_spec.rb
+++ 20220405/game_spec.rb
@@ -22,13 +22,5 @@ module D20220405
       roll_many(20, 1)
       expect(@game.score).to eq 20
     end
-
-    it 'when one spare' do
-      @game.roll(5)
-      @game.roll(5)
-      @game.roll(3)
-      roll_many(17, 0)
-      expect(@game.score).to eq 16
-    end
   end
 end
