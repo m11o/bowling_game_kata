48f1897f6d668ea345881bb054be9cf88879df67
Masahito Osako <velonica.gale2227@gmail.com>
2022-04-06 22:53:43 +0900

    complete in 20220406

---
+++ 20220406/game_spec.rb
@@ -0,0 +1,55 @@
+require_relative '../spec/spec_helper'
+require_relative '../20220406/game'
+
+module D20220406
+  describe '20220406' do
+    before do
+      @game = Game.new
+    end
+
+    def roll_many(rolls, pins)
+      rolls.times do
+        @game.roll(pins)
+      end
+    end
+
+    def roll_spare
+      @game.roll(5)
+      @game.roll(5)
+    end
+
+    def roll_strike
+      @game.roll(10)
+    end
+
+    it 'when all gutters' do
+      roll_many(20, 0)
+      expect(@game.score).to eq 0
+    end
+
+    it 'when all one point' do
+      roll_many(20, 1)
+      expect(@game.score).to eq 20
+    end
+
+    it 'when one spare' do
+      roll_spare
+      @game.roll(3)
+      roll_many(17, 0)
+      expect(@game.score).to eq 16
+    end
+
+    it 'when one strike' do
+      roll_strike
+      @game.roll(4)
+      @game.roll(3)
+      roll_many(16, 0)
+      expect(@game.score).to eq 24
+    end
+
+    it 'when perfect game' do
+      12.times { @game.roll(10) }
+      expect(@game.score).to eq 300
+    end
+  end
+end
