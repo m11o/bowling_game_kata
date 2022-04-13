36e9e8b6cde65a6073e587a6c3a107f03383257a
m11o <m11o@hey.jp>
2022-04-13 09:20:51 +0900

    add 20220413

---
+++ 20220413/game.rb
@@ -0,0 +1,43 @@
+module D20220413
+  class Game
+    attr_reader :score
+
+    def initialize
+      @rolls = []
+      @current_frame = 0
+    end
+
+    def roll(pins)
+      @rolls[@current_frame] = pins
+      @current_frame += 1
+    end
+
+    def score
+      score = 0
+      roll_index = 0
+      10.times do
+        if strike? roll_index
+          score += 10 + @rolls[roll_index + 1] + @rolls[roll_index + 2]
+          roll_index += 1
+        elsif spare?(roll_index)
+          score += 10 + @rolls[roll_index + 2]
+          roll_index += 2
+        else
+          score += @rolls[roll_index] + @rolls[roll_index + 1]
+          roll_index += 2
+        end
+      end
+      score
+    end
+
+    private
+
+    def spare?(roll_index)
+      @rolls[roll_index] + @rolls[roll_index + 1] == 10
+    end
+
+    def strike?(roll_index)
+      @rolls[roll_index] == 10
+    end
+  end
+end
